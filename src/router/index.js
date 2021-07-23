74173a14f193ecdf95280e9b435dd08cb602bab7
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-23 17:53:42 -0300

    feat(router/index.js): route guardian added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/pages/home'
+import Login from '../views/pages/login'
 
 Vue.use(VueRouter)
 
@@ -8,15 +9,15 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
@@ -24,6 +25,21 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    const loggedIn = localStorage.getItem('token')
+
+    if (loggedIn) {
+      next()
+      return
+    }
+
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router

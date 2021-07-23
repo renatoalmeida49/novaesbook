54299f17e6a87dfffb7c139b4729b88fd25a06bf
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-23 19:28:05 -0300

    feat(store/index): module of user created

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,40 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  state() {
+    return {
+      token: localStorage.getItem('token') || '',
+      user: {}
+    }
   },
   mutations: {
+    auth_success(state, payload) {
+      console.log('Mutation payload: ', payload.user)
+      state.user = payload.user
+    }
   },
   actions: {
+    login(context, payload) {
+      this.$http.post('http://localhost:5000/users/sign-in', {
+        email: payload.email,
+        password: payload.password
+      })
+        .then(response => {
+          const token = response.data.token
+          const user = response.data.user
+
+          localStorage.setItem('token', token)
+
+          payload.user = user
+
+          context.commit('login', payload)
+        })
+      
+    }
   },
-  modules: {
+  getters: {
+    user(state) {
+      return state.user
+    }
   }
 })

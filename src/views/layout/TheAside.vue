84c4602e39815eb3dffe3c919e7a01efd2c176ad
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-24 11:49:55 -0300

    feat(layout/TheAside.vue): structure for aside menu created

---
+++ src/views/layout/TheAside.vue
@@ -0,0 +1,19 @@
+<template>
+<aside>
+    <div>Menu lateral</div>
+</aside>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+aside {
+    background: red;
+    width: 190px;
+}
+
+</style>

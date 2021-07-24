e1167bc7d298cfed86a03aa7245acb5c67510c93
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-24 10:19:24 -0300

    build(layout/): folder to keep all the components used in layout

---
+++ src/views/layout/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+      <div class="container">
+
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "TheHeader"
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+    background-color: #4a76a8;
+    height: 45px;
+    box-shadow: 0px 0px 4px #333;
+    position: relative;
+    z-index: 99;
+
+    .container {
+        display: flex;
+        height: inherit;
+        max-width: 1040px;
+        margin: auto;
+    }
+}
+</style>

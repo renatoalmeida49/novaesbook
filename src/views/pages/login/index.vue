8c2150c8000ba6aa103aa5f892ba3929cef8dbcd
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-24 20:13:38 -0300

    feat(login/index.vue): create new accounts

--- src/views/pages/login/index.vue
+++ src/views/pages/login/index.vue
@@ -6,29 +6,84 @@
       </div>
   </header>
   
-  <div class="form-login">
-      <input type="email" placeholder="Digite seu e-mail" v-model="credentials.email">
+    <div class="form-login">
+        <input
+            v-if="newAccountFlag"
+            type="text"
+            placeholder="Digite seu nome completo"
+            v-model="newAccountData.name"
+        >
 
-      <input type="password" placeholder="Digite sua senha" v-model="credentials.password">
+        <input
+            type="email"
+            placeholder="Digite seu e-mail"
+            v-model="credentials.email"
+        >
 
-      <button @click="submitLogin">Acessar</button>
+        <input
+            type="password"
+            placeholder="Digite sua senha"
+            v-model="credentials.password"
+        >
 
-      <a href="">Ainda não tem conta? Cadastre-se</a>
+        <input
+            v-if="newAccountFlag"
+            type="text"
+            placeholder="Digite sua data de nascimento"
+            v-model="newAccountData.birthdate"
+        >
+
+        <button
+            v-if="newAccountFlag"
+            class="btn-access"
+            @click="submitNewAccount"
+        >
+            Criar conta
+        </button>
+
+        <button
+            v-else
+            class="btn-access"
+            @click="submitLogin"
+        >
+            Acessar
+        </button>
+
+        <button
+            v-if="newAccountFlag"
+            class="btn-new-access"
+            @click="newAccount"
+        >
+            Já tem conta? Faça login
+        </button>
+        <button
+            v-else
+            class="btn-new-access"
+            @click="newAccount"
+        >
+            Ainda não tem conta? Cadastre-se
+        </button>
   </div>
 </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import axios from 'axios'
 
 export default {
     name: "Login",
     data() {
         return {
+            newAccountData: {
+                name: '',
+                birthdate: ''
+            },
             credentials: {
                 email: '',
                 password: ''
-            }
+            },
+            newAccountFlag: false
         }
     },
     methods: {
@@ -40,6 +95,26 @@ export default {
             if (localStorage.getItem('token') != null) {
                 this.$router.push({ name: 'Home'})
             }
+        },
+
+        newAccount() {
+            this.newAccountFlag = !this.newAccountFlag
+        },
+
+        submitNewAccount() {
+            axios.post('http://localhost:4000/users/sign-up', {
+                name: this.newAccountData.name,
+                email: this.credentials.email,
+                password: this.credentials.password,
+                birthdate: this.newAccountData.birthdate
+            })
+                .then(() => {
+                    this.submitLogin()
+                })
+                .catch(error => {
+                    console.log("Erro!")
+                    console.error(error)
+                })
         }
     }
 }
@@ -81,7 +156,7 @@ header {
         outline: none;
     }
 
-    button {
+    .btn-access {
         border: 0;
         padding: 10px 20px;
         background-color: #4a76a8;
@@ -97,9 +172,12 @@ header {
         margin-bottom: 10px;
     }
 
-    a {
+    .btn-new-access {
         text-decoration: none;
         color: #4a76a8;
+        border: none;
+        background: transparent;
+        cursor: pointer;
     }
 }
 </style>

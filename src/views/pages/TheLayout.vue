27f16c2e75506a75788dcd2fb9da8656153749ae
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-24 10:18:56 -0300

    feat(TheLayout.vue): structure and router-view used for the whole project added

---
+++ src/views/pages/TheLayout.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <TheHeader />
+
+    <router-view></router-view>
+</div>
+</template>
+
+<script>
+import TheHeader from "@/views/layout/TheHeader"
+
+export default {
+    name: "TheLayout",
+    components: {
+        TheHeader
+    }
+}
+</script>
+
+<style>
+
+</style>

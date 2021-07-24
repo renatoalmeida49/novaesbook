e4b32630307ea82de8d2188a5cf592178e9c8535
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-24 11:51:33 -0300

    feat(layout/TheColumn.vue): structure to right column created

---
+++ src/views/layout/TheColumn.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="column">
+    A outra coluna
+</div>
+</template>
+
+<script>
+export default {
+    name: "TheColumn"
+}
+</script>
+
+<style lang="scss" scoped>
+.column {
+    background: blue;
+    color: white;
+    width: 250px;
+}
+</style>

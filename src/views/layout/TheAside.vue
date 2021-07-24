2303ab9b19dc14a894748913f65d9fe283235d0f
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-24 12:04:10 -0300

    feat(TheAside.vue): tag nav, ul and icons added

--- src/views/layout/TheAside.vue
+++ src/views/layout/TheAside.vue
@@ -1,6 +1,34 @@
 <template>
-<aside>
-    <div>Menu lateral</div>
+<aside class="aside">
+    <nav class="nav">
+        <ul>
+            <li>
+            <img src="@/assets/icons/home-run.png" width="16" height="16" alt="Home icon">
+                Home
+            </li>
+            <li>
+                <img src="@/assets/icons/user.png" width="16" height="16" alt="Home icon">
+                Meu perfil
+            </li>
+            <li>
+                <img src="@/assets/icons/friends.png" width="16" height="16" alt="Home icon">
+                Amigos
+            </li>
+            <li>
+                <img src="@/assets/icons/photo.png" width="16" height="16" alt="Home icon">
+                Fotos
+            </li>
+            <div>(Spliter</div>
+            <li>
+                <img src="@/assets/icons/settings.png" width="16" height="16" alt="Home icon">
+                Configurações
+            </li>
+            <li>
+                <img src="@/assets/icons/power.png" width="16" height="16" alt="Home icon">
+                Sair
+            </li>
+        </ul>
+    </nav>
 </aside>
 </template>
 
@@ -11,9 +39,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-aside {
-    background: red;
+.aside {
     width: 190px;
+    color: #224b7a;
+    text-align: left;
+
+    .nav {
+        ul {
+            list-style: none;
+        }
+    }
 }
 
 </style>

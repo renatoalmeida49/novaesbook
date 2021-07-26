4fa43f0dcb903ae54dff0b65483a22f5ac1f2caf
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-26 11:52:11 -0300

    feat(NotFound.vue): component created

---
+++ src/views/pages/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Nothing to see here...</div>
+</template>
+
+<script>
+export default {
+    name: "NotFound",
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+    text-align: center;
+    width: 100%;
+    font-size: 20px;
+}
+</style>

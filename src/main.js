a9ecbce444b8c6b3532dc34d6ec50fe22209ecd1
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-27 16:01:08 -0300

    feat(main.js): axios now available to all components

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { api } from './services/api'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = api
 
 new Vue({
   router,

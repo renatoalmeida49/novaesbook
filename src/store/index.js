1e23f6a80b42059cd279ef5bc8f0b3f5280c187a
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-28 18:47:54 -0300

    feat(store/index.js): module post added

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 import user from './modules/user.module'
+import post from './modules/post.module'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    user
+    user,
+    post
   },
   plugins: [
     createPersistedState({

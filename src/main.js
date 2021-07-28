aeb8d44ccdfc16d2402189b3f069ba7e754e80da
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-28 07:14:04 -0300

    feat(main.js): filters imported

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { api } from './services/api'
+import './filters'
 
 Vue.config.productionTip = false
 

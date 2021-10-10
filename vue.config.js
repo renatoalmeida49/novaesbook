2cd877830c15420797b3a846d7f8d49a64332a7b
Renato Novaes <renatoalmeida49@gmail.com>
2021-10-10 07:07:51 -0300

    feat(vue.config.js): page title and scss file

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-    devServer: {
-        host: 'localhost',
-        port: 3131,
-        https: false
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: '@import "@/assets/scss/app.scss";'
+      }
     }
+  },
+  devServer: {
+    host: 'localhost',
+    port: 3131,
+    https: false
+  },
 }

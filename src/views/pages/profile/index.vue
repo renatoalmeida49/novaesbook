dc5fe20c6fb3e5d13a02568d059cb1333ea409df
Renato Novaes <renatoalmeida49@gmail.com>
2021-10-11 10:41:42 -0300

    style(profile.vue) : tab spacing and removed vue scoped

--- src/views/pages/profile/index.vue
+++ src/views/pages/profile/index.vue
@@ -1,224 +1,235 @@
 <template>
-<div class="profile-content">
+
+  <div class="profile-content">
     <div class="header">
-        <div class="cover" :style="getBackground"></div>
-        <div class="profile-info">
-            <div class="photo-name">
-                <img src="@/assets/images/default-avatar.jpg" width="50" height="50" alt="Profile picture">
-                <router-link :to="{ name: 'UserProfile', params: { userId: userProfile.userToShow.id } }">
-                    <p class="name">{{ userProfile.userToShow.name }}</p>
-                </router-link>
-            </div>
-
-            <div class="numbers">
-                <button class="button-relation" v-if="checkButton" @click="relation">
-                    {{ textButton }}
-                </button>
-
-                <div>
-                    <router-link :to="{ name: 'Friends', query: { list: 'followers'}, params: { userId: getParam }  }">
-                        <span>{{ userProfile.followers.length }}</span>
-                        <p>Seguidores</p>
-                    </router-link>
-                </div>
-                <div>
-                    <router-link :to="{ name: 'Friends', query: { list: 'following' }, params: { userId: getParam } }">
-                        <span>{{ userProfile.following.length }}</span>
-                        <p>Seguindo</p>
-                    </router-link>
-                </div>
-                <div>
-                    <span>4</span>
-                    <p>Fotos</p>
-                </div>
-            </div>
+      <div class="cover" :style="getBackground"></div>
+      <div class="profile-info">
+        <div class="photo-name">
+          <img src="@/assets/images/default-avatar.jpg" width="50" height="50" alt="Profile picture">
+          <router-link :to="{ name: 'UserProfile', params: { userId: userProfile.userToShow.id } }">
+            <p class="name">{{ userProfile.userToShow.name }}</p>
+          </router-link>
         </div>
+
+        <div class="numbers">
+          <button class="button-relation" v-if="checkButton" @click="relation">
+            {{ textButton }}
+          </button>
+
+          <div>
+            <router-link :to="{ name: 'Friends', query: { list: 'followers'}, params: { userId: getParam }  }">
+              <span>{{ userProfile.followers.length }}</span>
+              <p>Seguidores</p>
+            </router-link>
+          </div>
+          <div>
+            <router-link :to="{ name: 'Friends', query: { list: 'following' }, params: { userId: getParam } }">
+              <span>{{ userProfile.following.length }}</span>
+              <p>Seguindo</p>
+            </router-link>
+          </div>
+          <div>
+            <span>4</span>
+            <p>Fotos</p>
+          </div>
+        </div>
+      </div>
     </div>
 
     <router-view :user="userProfile"/>
-</div>
+  </div>
+
 </template>
 
+
 <script>
+
 import { mapGetters, mapActions } from 'vuex'
-import { api } from '@/services/api'
 
 export default {
-    name: "Profile",
-    data() {
-        return {
-            userProfile: {
-                userToShow: {},
-                postsToShow: [],
-                isFollowing: false,
-                following: 0,
-                followers: 0,
-            }
-        }
-    },
-    mounted() {
-        if(this.$route.params.userId) {
-            this.getProfile(this.$route.params.userId)
-        } else {
-            this.getProfile(this.user.id)
-        }
-    },
-    computed: {
-        ...mapGetters('user', ['user']),
-        ...mapGetters('post', ['posts']),
-        ...mapGetters('relation', ['following', 'followers']),
+  name: "Profile",
 
-        getBackground() {
-            return {
-                backgroundImage: `url(${require('@/assets/images/default-cover.jpg')}`
-            }
-        },
-
-        checkButton() {
-            if (this.$route.params.userId) {
-                return !(this.$route.params.userId == this.user.id)
-            } else {
-                return false
-            }
-        },
-
-        textButton() {
-            return this.userProfile.isFollowing ? 'Deixar de seguir' : 'Seguir'
-        },
-
-        getParam() {
-            return this.$route.params.userId
-        }
-    },
-    watch: {
-        '$route.params'() {
-            this.getProfile(this.$route.params.userId)
-        }
-    },
-    methods: {
-        ...mapActions('relation', ['updateRelation']),
-        
-        getProfile(id) {
-            if (id != this.user.id && id != null) {
-                api.post('/users/profile', {
-                        id: id
-                    })
-                    .then(response => {
-                        this.userProfile.userToShow = response.data.user
-                        this.userProfile.postsToShow = response.data.posts
-                        this.userProfile.following = Array.from(response.data.following).map(item => {
-                            return item.to
-                        })
-                        this.userProfile.followers = Array.from(response.data.followers).map(item => {
-                            return item.from
-                        })
-
-                        this.getRelation(id)
-                    })
-            } else {
-                this.userProfile.userToShow = this.user
-                this.userProfile.postsToShow = this.posts
-                this.userProfile.following = this.following
-                this.userProfile.followers = this.followers
-            }
-        },
-
-        relation() {
-            this.updateRelation({
-                id: this.$route.params.userId,
-                user: this.userProfile.userToShow,
-                flag: this.userProfile.isFollowing
-            })
-                .then(() => {
-                    this.userProfile.isFollowing = !this.userProfile.isFollowing
-                })
-        },
-
-        getRelation(id) {
-            const search = this.following.filter(user => {
-                return user.id == id
-            })
-
-            search.length == 0 ? this.userProfile.isFollowing = false : this.userProfile.isFollowing = true
-        }
+  data() {
+    return {
+      userProfile: {
+        userToShow: {},
+        postsToShow: [],
+        isFollowing: false,
+        following: 0,
+        followers: 0,
+      }
     }
+  },
+
+  mounted() {
+    if(this.$route.params.userId) {
+      this.getProfile(this.$route.params.userId)
+    } else {
+      this.getProfile(this.user.id)
+    }
+  },
+
+  computed: {
+    ...mapGetters('user', ['user']),
+    ...mapGetters('post', ['posts']),
+    ...mapGetters('relation', ['following', 'followers']),
+
+    getBackground() {
+      return {
+        backgroundImage: `url(${require('@/assets/images/default-cover.jpg')}`
+      }
+    },
+
+    checkButton() {
+      if (this.$route.params.userId) {
+        return !(this.$route.params.userId == this.user.id)
+      } else {
+        return false
+      }
+    },
+
+    textButton() {
+      return this.userProfile.isFollowing ? 'Deixar de seguir' : 'Seguir'
+    },
+
+    getParam() {
+      return this.$route.params.userId
+    }
+  },
+
+  watch: {
+    '$route.params'() {
+      this.getProfile(this.$route.params.userId)
+    }
+  },
+
+  methods: {
+    ...mapActions('relation', ['updateRelation']),
+      
+    getProfile(id) {
+      if (id != this.user.id && id != null) {
+        this.$api.post('/users/profile', {
+            id: id
+          })
+          .then(response => {
+            this.userProfile.userToShow = response.data.user
+            this.userProfile.postsToShow = response.data.posts
+            this.userProfile.following = Array.from(response.data.following).map(item => {
+              return item.to
+            })
+            this.userProfile.followers = Array.from(response.data.followers).map(item => {
+              return item.from
+            })
+
+            this.getRelation(id)
+          })
+      } else {
+        this.userProfile.userToShow = this.user
+        this.userProfile.postsToShow = this.posts
+        this.userProfile.following = this.following
+        this.userProfile.followers = this.followers
+      }
+    },
+
+    relation() {
+      this.updateRelation({
+        id: this.$route.params.userId,
+        user: this.userProfile.userToShow,
+        flag: this.userProfile.isFollowing
+      })
+        .then(() => {
+          this.userProfile.isFollowing = !this.userProfile.isFollowing
+        })
+    },
+
+    getRelation(id) {
+      const search = this.following.filter(user => {
+        return user.id == id
+      })
+
+      search.length == 0 ? this.userProfile.isFollowing = false : this.userProfile.isFollowing = true
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+
+<style lang="scss">
+
 .profile-content {
-    width: 100%;
+  width: 100%;
 
-    .header {
-        margin-bottom: 10px;
+  .header {
+    margin-bottom: 10px;
 
-        .cover {
-            height: 250px;
-            background-size: cover;
-            background-position: center;
-        }
-
-        .profile-info {
-            background: #fff;
-            border: 1px solid #c9cacc;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding: 16px;
-            border-radius: 5px;
-
-            .photo-name {
-                display: flex;
-                align-items: center;
-
-                img {
-                    border-radius: 50%;
-                    margin-right: 20px;
-                }
-
-                .name {
-                    color: #000;
-                    font-size: 20px;
-                }
-
-                a {
-                    text-decoration: none;
-                }
-            }
-
-            .numbers {
-                display: flex;
-                gap: 50px;
-
-                .button-relation {
-                    border: 0;
-                    padding: 10px 20px;
-                    background-color: $primary-color;
-                    border-radius: 10px;
-                    color: #FFF;
-                    font-size: 15px;
-                    cursor: pointer;
-                    box-shadow: 0px 0px 3px #999;
-                    text-decoration: none;
-                    display: inline-block;
-                }
-
-                span {
-                    font-weight: bold;
-                    font-size: 20px;
-                    color: #224b7a;
-                }
-
-                p {
-                    font-size: 14px;
-                    color: #999;
-                }
-
-                a {
-                    text-decoration: none;
-                }
-            }
-        }
+    .cover {
+      height: 250px;
+      background-size: cover;
+      background-position: center;
     }
+
+    .profile-info {
+      background: #fff;
+      border: 1px solid #c9cacc;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 16px;
+      border-radius: 5px;
+
+      .photo-name {
+        display: flex;
+        align-items: center;
+
+        img {
+          border-radius: 50%;
+          margin-right: 20px;
+        }
+
+        .name {
+          color: #000;
+          font-size: 20px;
+        }
+
+        a {
+          text-decoration: none;
+        }
+      }
+
+      .numbers {
+        display: flex;
+        gap: 50px;
+
+        .button-relation {
+          border: 0;
+          padding: 10px 20px;
+          background-color: $primary-color;
+          border-radius: 10px;
+          color: #FFF;
+          font-size: 15px;
+          cursor: pointer;
+          box-shadow: 0px 0px 3px #999;
+          text-decoration: none;
+          display: inline-block;
+        }
+
+        span {
+          font-weight: bold;
+          font-size: 20px;
+          color: #224b7a;
+        }
+
+        p {
+          font-size: 14px;
+          color: #999;
+        }
+
+        a {
+          text-decoration: none;
+        }
+      }
+    }
+  }
 }
+
 </style>
--- src/views/pages/settings/index.vue
+++ src/views/pages/settings/index.vue
@@ -1,111 +1,123 @@
 <template>
-<div class="settings-content">
+
+  <div class="settings-content">
     <div class="form">
-        <label>Avatar:</label>
-        <input type="file">
+      <label>Avatar:</label>
+      <input type="file">
 
-        <label>Cover:</label>
-        <input type="file">
+      <label>Cover:</label>
+      <input type="file">
 
-        <label>Nome completo:</label>
-        <input type="text" v-model="userInfo.name">
+      <label>Nome completo:</label>
+      <input type="text" v-model="userInfo.name">
 
-        <label>Data de nascimento:</label>
-        <input type="text" v-model="userInfo.birthdate">
+      <label>Data de nascimento:</label>
+      <input type="text" v-model="userInfo.birthdate">
 
-        <label>E-mail:</label>
-        <input type="text" v-model="userInfo.email">
+      <label>E-mail:</label>
+      <input type="text" v-model="userInfo.email">
 
-        <label>Cidade:</label>
-        <input type="text" v-model="userInfo.city">
+      <label>Cidade:</label>
+      <input type="text" v-model="userInfo.city">
 
-        <label>Trabalho:</label>
-        <input type="text" v-model="userInfo.work">
+      <label>Trabalho:</label>
+      <input type="text" v-model="userInfo.work">
 
-        <label>Nova senha:</label>
-        <input type="text" disabled>
+      <label>Nova senha:</label>
+      <input type="text" disabled>
 
-        <label>Confirmar senha:</label>
-        <input type="text" disabled>
+      <label>Confirmar senha:</label>
+      <input type="text" disabled>
 
-        <button @click="submitUpdate">Atualizar informações</button>
+      <button @click="submitUpdate">Atualizar informações</button>
     </div>
-</div>
+  </div>
+
 </template>
 
+
 <script>
+
 import { mapGetters, mapActions } from 'vuex'
 
 export default {
-    name: "Settings",
-    data() {
-        return {
-            userInfo: {}
-        }
-    },
-    mounted() {
-        this.userInfo = this.user
-    },
-    computed: {
-        ...mapGetters('user', ['user'])
-    },
-    methods: {
-        ...mapActions('user', ['update']),
+  name: "Settings",
 
-        async submitUpdate() {
-            await this.update(this.userInfo)
-        }
+  data() {
+    return {
+      userInfo: {}
     }
+  },
+
+  mounted() {
+    this.userInfo = this.user
+  },
+
+  computed: {
+    ...mapGetters('user', ['user'])
+  },
+
+  methods: {
+    ...mapActions('user', ['update']),
+
+    async submitUpdate() {
+      await this.update(this.userInfo)
+    }
+  }
 }
+
 </script>
 
-<style lang="scss" scoped>
+
+<style lang="scss">
+
 .settings-content {
+  margin: auto;
+  text-align: start;
+
+  .form {
+    max-width: 400px;
     margin: auto;
-    text-align: start;
+    background-color: #FFF;
+    border: 1px solid #c9cacc;
+    border-radius: 5px;
+    padding: 20px;
+    margin-top: 50px;
 
-    .form {
-        max-width: 400px;
-        margin: auto;
-        background-color: #FFF;
-        border: 1px solid #c9cacc;
-        border-radius: 5px;
-        padding: 20px;
-        margin-top: 50px;
-
-        label {
-            display: block;
-            font-size: 13px;
-            margin-bottom: 2px;
-        }
-
-        input {
-            font-size: 15px;
-            height: 40px;
-            width: 100%;
-            outline: 0;
-            border: 0;
-            background-color: #EEE;
-            border-radius: 10px;
-            padding: 15px;
-            margin-bottom: 10px;
-        }
-
-        button {
-            border: 0;
-            padding: 10px 20px;
-            background-color: $primary-color;
-            border-radius: 10px;
-            color: #FFF;
-            font-size: 15px;
-            margin-bottom: 10px;
-            cursor: pointer;
-            box-shadow: 0px 0px 3px #999;
-            width: 100%;
-            
-            margin: auto;
-            margin-bottom: 10px;
-        }
+    label {
+      display: block;
+      font-size: 13px;
+      margin-bottom: 2px;
     }
+
+    input {
+      font-size: 15px;
+      height: 40px;
+      width: 100%;
+      outline: 0;
+      border: 0;
+      background-color: #EEE;
+      border-radius: 10px;
+      padding: 15px;
+      margin-bottom: 10px;
+    }
+
+    button {
+      border: 0;
+      padding: 10px 20px;
+      background-color: $primary-color;
+      border-radius: 10px;
+      color: #FFF;
+      font-size: 15px;
+      margin-bottom: 10px;
+      cursor: pointer;
+      box-shadow: 0px 0px 3px #999;
+      width: 100%;
+      
+      margin: auto;
+      margin-bottom: 10px;
+    }
+  }
 }
+
 </style>
